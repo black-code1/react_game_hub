7ef07990259d08db29e01433d08bbda20b82486c
black-code1 <legrandmahamat1@gmail.com>
2023-11-09 08:53:08 +0100

    displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,6 +12,8 @@ export interface Game {
     name: string;
     background_image: string;
     parent_platforms: { platform: Platform }[];
+    metacritic: number;
+
 }
 interface FetchGamesResponse {
     count: number;

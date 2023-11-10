eda966d37e0fc7be35adc2fbdc3d90acf56c3cb7
black-code1 <legrandmahamat1@gmail.com>
2023-11-10 13:16:35 +0100

    building for production

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -4,13 +4,14 @@ import {SiNintendo} from "react-icons/si";
 import {BsGlobe} from "react-icons/bs";
 import {Platform} from "../hooks/useGames";
 import {HStack, Icon} from "@chakra-ui/react";
+import {IconType} from "react-icons";
 
 interface Props {
     platforms: Platform[]
 }
 
-const PlatformIconList = ({platforms}: Props) => {
-    const iconMap = {
+const PlatformIconList = ({platforms = []}: Props) => {
+    const iconMap: {[key: string]: IconType} = {
        pc: FaWindows,
        playstation: FaPlaystation,
        xbox: FaXbox,
